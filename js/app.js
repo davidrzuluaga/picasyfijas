0981a782dbbfc7ef3a09b71022e563563713f175
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-11 02:50:30 -0500

    avance de la funcionalidad

--- js/app.js
+++ js/app.js
@@ -8,23 +8,56 @@ function numbergenerate () {
     return random4numbers;
 };
 
-numbergenerate();
+var numbermachine = numbergenerate();
+console.log(numbermachine);
 
 $('input').on('keyup', function (e) {
-    numberarray = $(this).val().split("");
-    
-    for (i=0; i < numberarray.length; i++) {
-        numberarray[i] != numberarray[0]
-        numberarray[i] != numberarray[1]
-        numberarray[i] != numberarray[2]
-        numberarray[i] != numberarray[3]
+    var number = $(this).val();
+    var numberarray = number.split("");
+
+    function unique4 (){
+        sorted_arr = numberarray.slice().sort(); 
+        results = [];
+        for (var i = 0; i < sorted_arr.length - 1; i++) {
+            if (sorted_arr[i + 1] == sorted_arr[i]) {
+                results.push(sorted_arr[i]);
+            }
+        }
+        return results.length == 0;
     }
 
    if (e.which == 13) {
-       if (numberarray.length == 4) {
-        $('.howto span').css('color', 'black');           
-        $('.table').show();
-        
+        if (numberarray.length == 4 && unique4()) {
+            function fijas (){
+               fijas = 0;
+               for (i = 0; i < 4; i++){
+                   numberarray[i] == numbermachine[i] ? fijas++ : 0;
+               }
+               return fijas;
+            };
+
+            function picas (){
+                picas = 0;
+                for (i=0; i < 4; i++){
+                    var even = function(element) {
+                        return element == numbermachine[i];
+                      };
+                    numberarray.some(even) ? picas++ : 0;
+                };
+                return picas;
+            };
+           
+
+            $('.howto span').css('color', 'black');           
+            $('.table').show();
+
+            $('tbody').append('<tr>');           
+            $('tbody').append('<td>'+number+'<td>');
+            $('tbody').append('<td>'+Math.abs((+fijas())-(+picas()))+'</td>');
+            $('tbody').append('<td>'+fijas+'</td>');
+            $('tbody').append('</tr>');
+            
+            $('input').val("");
        } else {
            $('.howto span').css('color', 'red');
        }
